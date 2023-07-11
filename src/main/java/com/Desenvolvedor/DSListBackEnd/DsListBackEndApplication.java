727eae882d794081cf7a8917fd524a10e120b66e
jucelio farias coelho <juceliocoelho2022@gmail.com>
2023-07-11 17:17:19 -0300

    Project created

---
+++ src/main/java/com/Desenvolvedor/DSListBackEnd/DsListBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.Desenvolvedor.DSListBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsListBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsListBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Desenvolvedor/DSListBackEnd/DsListBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Desenvolvedor.DSListBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsListBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
